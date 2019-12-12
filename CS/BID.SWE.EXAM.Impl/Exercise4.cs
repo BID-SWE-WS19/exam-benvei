20a0ce02da86194d1a42e8a61f4b74cab5f2e45e
Benjamin Veitschegger <benjamin@benvei.at>
2019-12-12 16:14:02 +0100

    Exercise 4 methode one done

--- CS/BID.SWE.EXAM.Impl/Exercise4.cs
+++ CS/BID.SWE.EXAM.Impl/Exercise4.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using BID.SWE1.Exam.Interfaces;
 
 namespace BID.SWE.EXAM.Impl
@@ -7,7 +9,19 @@ namespace BID.SWE.EXAM.Impl
     {
         public object Method1()
         {
-            throw new NotImplementedException();
+            UnicodeEncoding uniEncoding = new UnicodeEncoding();
+            byte[] mystring = uniEncoding.GetBytes(" Frohe Weihnachten ");
+            byte[] myint = uniEncoding.GetBytes("42");
+            byte[] mybool = uniEncoding.GetBytes("false");
+
+            MemoryStream mystream = new MemoryStream();
+            mystream.Write(mystring, 0, mystring.Length);
+            int offset = mystring.Length;
+            mystream.Write(myint, offset, myint.Length);
+            offset += myint.Length;
+            mystream.Write(mybool, offset, mybool.Length);
+
+            return mystream;
         }
 
         public object Method2(object obj)
