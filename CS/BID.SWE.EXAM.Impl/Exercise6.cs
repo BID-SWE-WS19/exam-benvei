a13cc4742f73a045ea8d1da64ec1961e512508de
Benjamin Veitschegger <benjamin@benvei.at>
2019-12-12 16:38:38 +0100

    Exercise 6 done, Exercise 5 work in progress

--- CS/BID.SWE.EXAM.Impl/Exercise6.cs
+++ CS/BID.SWE.EXAM.Impl/Exercise6.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using BID.SWE1.Exam.Interfaces;
 
 namespace BID.SWE.EXAM.Impl
@@ -7,7 +8,24 @@ namespace BID.SWE.EXAM.Impl
     {
         public object Method1()
         {
-            throw new NotImplementedException();
+            string name = "Benjamin";
+            int stunde = 17;
+            int minute = 00;
+            int tag = 12;
+            int monat = 12;
+            int jahr = 2019;
+
+
+            string myQuery = "insert into TestAbgaben (ID, Tag, Monat, Jahr, Stunde, Minute, Name) VALUES (NEWID(), @tag, @monat, @jahr, @stunde, @minute, @name)";
+            SqlCommand command = new SqlCommand(myQuery);
+            command.Parameters.AddWithValue("@tag", tag);
+            command.Parameters.AddWithValue("@monat", monat);
+            command.Parameters.AddWithValue("@jahr", jahr);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@Stunde", stunde);
+            command.Parameters.AddWithValue("@Minute", minute);
+            return command;
         }
     }
+
 }
